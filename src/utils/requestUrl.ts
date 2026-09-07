a12839750a22cb3542b0ca779b0ded2d20cea6f5
dak <dak@local>
2026-09-06 21:21:36 -0400

    Preserve full host for request links

--- src/utils/requestUrl.ts
+++ src/utils/requestUrl.ts
@@ -9,5 +9,5 @@ export function getRequestHref() {
         return `${protocol}//${hostname}${port}`;
     }
 
-    return `${protocol}//request.${labels.slice(-2).join('.')}`;
+    return `${protocol}//request.${hostname}`;
 }
